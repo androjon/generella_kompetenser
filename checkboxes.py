3da29a37c77c5d7809aa612ee45f1ac703940f7d
androjon <jonfindahl@gmail.com>
2024-10-15 19:41:53 +0200

    Update checkboxes.py

--- checkboxes.py
+++ checkboxes.py
@@ -12,21 +12,20 @@ number_of_selected = 0
 st.session_state.choosen_generic_skills = []
 
 generic_data = läsa_in_json_fil("generic_skill_groups.json")
-st.header("Generella kompetenser")
-st.write("Kompetenser som gäller i princip alla yrken. Ofta kallas de också mjuka kompetenser eller soft skills. Här kan du du utforska och förstå de generella kompetenserna. Kan vara viktiga att lyfta fram i ett CV då många arbetsgivare värderar dessa högt.")
-st.write("Välj tre stycken generella kompetenser som kännetecknar dig mest i olika jobbsituationer.")
+st.header("**Generella kompetenser**")
+st.write("Kompetenser som gäller i princip alla yrken. Ofta kallas de också mjuka kompetenser eller soft skills. Här kan du du utforska och förstå de generella kompetenserna. Kan vara viktiga att lyfta fram i ett CV då många arbetsgivare värderar dessa högt.  \n\nVälj tre stycken generella kompetenser som kännetecknar dig mest i olika jobbsituationer.")
+
 st.divider()
 
-all_generic_skills_keys_definition = {}
+st.session_state.all_generic_skills_keys_definition = {}
 
 for i in generic_data:
     number_of_generic_skills = 0
-    st.write(i["name"])
-    st.write(i["defintion"])
+    st.write(f"**{i['name']}**  - {i['defintion']}")
     col1, col2 = st.columns(2)
     for g in i["generic_skills"]:
         keyname = g['label'][0]
-        all_generic_skills_keys_definition[g['label'][0]] = [g['name'], g['definition']]
+        st.session_state.all_generic_skills_keys_definition[g['label'][0]] = [g['name'], g['definition']]
         if (number_of_generic_skills % 2) == 0:
             col1.checkbox(f"{g['label'][0]}", help = f"{g['name']}  \n{g['definition']}", key = keyname)
         else:
@@ -34,23 +33,32 @@ for i in generic_data:
         number_of_generic_skills += 1
     st.divider()
 
-st.button("Spara din val och se om dina val kan hjälpa dig med ett CV")
+def save_selected_generic():
+    for key in list(st.session_state.all_generic_skills_keys_definition.keys()):
+        if st.session_state[key]:
+            text_input_key = f"input_{key}"
+            if st.session_state[text_input_key]:
+                selected = {st.session_state.all_generic_skills_keys_definition[key][0]: st.session_state[text_input_key]}
+            else:
+                selected = {st.session_state.all_generic_skills_keys_definition[key][0]: None}
+            st.session_state.choosen_generic_skills.append(selected)
+    with st.sidebar:
+        st.write(st.session_state.choosen_generic_skills)
+
+st.button("Spara din val och se om dina val kan hjälpa dig med ett CV", on_click = save_selected_generic)
 
 st.write("Det är inte ovanligt att arbetsgivare frågar efter hur du har hanterat olika jobbsituationer utifrån generella kompetenser.")
 col1, col2, col3 = st.columns(3)
 
-for key in list(all_generic_skills_keys_definition.keys()):
+for key in list(st.session_state.all_generic_skills_keys_definition.keys()):
     if st.session_state[key]:
-        definition = all_generic_skills_keys_definition.get(key)
+        definition = st.session_state.all_generic_skills_keys_definition.get(key)
         generic_skill_label = definition[0]
         number_of_selected += 1
-        if number_of_selected > 3:
-            st.write("Du valt fler än tre generella kompetenser. För att ändra dina val behöver du först ta bort något.")
-        elif number_of_selected == 1:
+        if (number_of_selected % 3) == 1:
             col1.text_area(f"{key}  \n{generic_skill_label}", value = "", key = f"input_{key}", height = 400, help = definition[1], placeholder = "Här kan du skriva konkreta exempel")
-        elif number_of_selected == 2:
+        elif (number_of_selected % 3) == 2:
             col2.text_area(f"{key}  \n{generic_skill_label}", value = "", key = f"input_{key}", height = 400, help = definition[1], placeholder = "Här kan du skriva konkreta exempel")
-        elif number_of_selected == 3:
+        else:
             col3.text_area(f"{key}  \n{generic_skill_label}", value = "", key = f"input_{key}", height = 400, help = definition[1], placeholder = "Här kan du skriva konkreta exempel")
-
-
+            
